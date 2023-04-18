03abbd2009d18a6f58c17ec580fd8c5fa5d52bd6
rosaforrestiana <30365715+rosaforrestiana@users.noreply.github.com>
2023-04-18 22:33:02 +0800

    Updated 18th April 2023

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 require('dotenv').config();
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const expect      = require('chai').expect;
-const cors        = require('cors');
+const express = require('express');
+const bodyParser = require('body-parser');
+const expect = require('chai').expect;
+const cors = require('cors');
 
-const fccTestingRoutes  = require('./routes/fcctesting.js');
-const apiRoutes         = require('./routes/api.js');
-const runner            = require('./test-runner');
+const fccTestingRoutes = require('./routes/fcctesting.js');
+const apiRoutes = require('./routes/api.js');
+const runner = require('./test-runner');
 
 const app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use(cors({ origin: '*' })); //For FCC testing purposes only
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //Index page (static HTML)
 app.route('/')
-  .get(function (req, res) {
+  .get(function(req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
   });
 
@@ -27,22 +27,22 @@ fccTestingRoutes(app);
 
 // User routes
 apiRoutes(app);
-    
+
 //404 Not Found Middleware
 app.use(function(req, res, next) {
   res.status(404)
     .type('text')
-    .send('Not Found');
+    .send('404 Not Found');
 });
 
 //Start our server and tests!
-const PORT = process.env.PORT || 3000
-app.listen(PORT, function () {
+const PORT = process.env.PORT || 3000;
+process.env.NODE_ENV
+app.listen(PORT, function() {
   console.log("Listening on port " + PORT);
-  // process.env.NODE_ENV='test'
-  if (process.env.NODE_ENV==='test') {
+  if (process.env.NODE_ENV === 'test') {
     console.log('Running Tests...');
-    setTimeout(function () {
+    setTimeout(function() {
       try {
         runner.run();
       } catch (error) {
