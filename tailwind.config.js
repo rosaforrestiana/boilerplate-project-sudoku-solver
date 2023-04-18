bddc676088dfd4150095d0fabaaab654adbe9eb5
rosaforrestiana <30365715+rosaforrestiana@users.noreply.github.com>
2023-04-18 22:41:08 +0800

    Updated 18th April 2023

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./views/*.{html,js}"],
+  theme: {
+    container: {
+      center: true,
+    },
+    extend: {},
+  },
+  plugins: [],
+}
